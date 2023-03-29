86ac018eb57f3b096024e9aeaea8de865c40bc0d
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-03-29 16:34:17 +0300

    3.7

--- part3/puhelinluettelon_backend/index.js
+++ part3/puhelinluettelon_backend/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+const unknownEndpoint = (request, response) => {
+response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
 app.use(express.json())
+app.use(requestLogger)
+app.use(morgan('tiny'))
 
 let persons = [
           {
@@ -82,7 +98,7 @@ app.get('/info', (req, res) => {
       response.json(person)
     })
 
-
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
