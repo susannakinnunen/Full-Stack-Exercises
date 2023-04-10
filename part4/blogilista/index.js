dfccf574f175828c671afb835742ba7c0f684eae
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-04-10 14:13:46 +0300

    add logger and config

--- part4/blogilista/index.js
+++ part4/blogilista/index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -15,7 +17,7 @@ const blogSchema = new mongoose.Schema({
 const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
-console.log("url", mongoUrl)
+logger.info("url", mongoUrl)
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -39,7 +41,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
