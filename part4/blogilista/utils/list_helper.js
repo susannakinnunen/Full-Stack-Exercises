e176aaecbf391d0bc1923be469f3597091c8c0ce
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-04-11 12:06:52 +0300

    4.5

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -10,9 +10,31 @@ const totalLikes = (blogs) => {
     return blogs.length === 0
     ? 0
     : blogs.reduce(reducer, 0)
-} 
+}
+
+const favouriteBlog = (blogs) => {
+    const reducer = (maxLikes, blog) => {
+        if ( blog.likes > maxLikes) {
+            maxLikes = blog.likes 
+            return maxLikes
+        } else {
+            return maxLikes
+        }
+    }
+
+    const maxLikes = blogs.reduce(reducer, 0)
+    const favBlog = blogs.filter(blog => blog.likes === maxLikes)
+
+    const result = {
+        title: favBlog[0].title,
+        author: favBlog[0].author,
+        likes: favBlog[0].likes
+    }
+    return result
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
   }
