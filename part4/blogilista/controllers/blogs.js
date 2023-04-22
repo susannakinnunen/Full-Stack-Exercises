1dfb69bb06942bcf58c2492e70d3be3bb7dffd59
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-04-22 15:25:07 +0300

    4.19

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  console.log("authorization", authorization)
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
@@ -9,8 +19,13 @@ blogsRouter.get('/', async (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-
-    const user = await User.findById(body.user)
+    console.log("body", body)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    console.log("decodedToken", decodedToken)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title: body.title,
