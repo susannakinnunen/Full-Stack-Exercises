edcefa970d8eebfac85cc3ece5c7b3f628c06ef2
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-05-05 11:21:39 +0300

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,21 +1,46 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    likes: 1,
-    author: 'Taina Testaaja',
-    title: 'Testing is fun',
-    url: 'www.testaus.fi' }
+describe('Blog tests', () => {
 
-  const mockAddLike = jest.fn()
-  const mockDeleteBlog = jest.fn()
+  beforeEach(() => {
+    const blog = {
+      likes: 1,
+      author: 'Taina Testaaja',
+      title: 'Testing is fun',
+      url: 'www.testaus.fi' }
 
-  render(<Blog blog={blog} addLike={mockAddLike} deleteBlog={mockDeleteBlog} />)
+    const mockAddLike = jest.fn()
+    const mockDeleteBlog = jest.fn()
 
-  const element = screen.getByText('Testing is fun Taina Testaaja')
-  screen.debug(element)
-  expect(element).toBeDefined()
+    render(<Blog blog={blog} addLike={mockAddLike} deleteBlog={mockDeleteBlog} />)
+
+  })
+
+  test('renders content', () => {
+    const element = screen.getByText('Testing is fun Taina Testaaja')
+    const url = screen.queryByText('www.testaus.fi')
+    const likes = screen.queryByText('1')
+    screen.debug(element)
+    expect(element).toBeDefined()
+    expect(url).toBeNull()
+    expect(likes).toBeNull()
+  })
+
+  test('renders content when view button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    const titleAndAuthor = screen.getByText('Testing is fun Taina Testaaja')
+    const url = screen.getByText('www.testaus.fi')
+    const likes = screen.getByText('1')
+    expect(titleAndAuthor).toBeDefined()
+    expect(url).toBeDefined()
+    expect(likes).toBeDefined()
+
+  })
+
 })
