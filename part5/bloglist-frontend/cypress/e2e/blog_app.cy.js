6724aadf32d21c955f292816307a5e06d796b85e
Susanna Kinnunen <susanna.kinnunen@windowslive.com>
2023-05-09 14:05:47 +0300

    5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,9 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', 'http://localhost:3003/api/users',
+      { username: 'mluukkai', password:'salainen'
+      })
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +11,27 @@ describe('Blog app', function() {
     cy.contains('Login')
     cy.contains('username')
     cy.contains('password')
+  })
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('mluukkai logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'mluukkai logged in')
+    })
   })
 })
